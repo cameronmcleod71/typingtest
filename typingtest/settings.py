e9609e160eb7137a8069a3c7f176ae1cb5113ffc
Cameron McLeod <cameronmcleod@Camerons-Air.gateway.sasktel.net>
2023-02-19 07:32:50 -0600

    everything is working for a basic char test with no users or communicaiton with a database

--- typingtest/settings.py
+++ typingtest/settings.py
@@ -78,8 +78,11 @@ WSGI_APPLICATION = 'typingtest.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'typerdb',
+        'PORT': '5432',
+        'USER': 'cam',
+        'PASSWORD': 'cam'
     }
 }
 
