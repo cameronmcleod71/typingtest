2efc5d919f3f9be2163914e3dd8be3cd22ef4a7f
Cameron McLeod <cameronmcleod@Camerons-Air.gateway.sasktel.net>
2023-06-09 15:22:54 -0600

    added logging

--- config/gunicorn.conf.py
+++ config/gunicorn.conf.py
@@ -1,2 +1,6 @@
 bind = "0.0.0.0:80"
 workers = 3
+accesslog = "/app/typingtest/logs/gunicorn.access.log"
+errorlog = "/app/typingtest/logs/gunicorn.app.log"
+capture_output = True
+loglevel = "info"
