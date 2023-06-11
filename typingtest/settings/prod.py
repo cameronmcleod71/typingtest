40749aef064fd5eb78df79b6b9b98081cd007315
Cameron McLeod <cameronmcleod@Camerons-Air.gateway.sasktel.net>
2023-06-10 19:46:46 -0600

    registered domain in django settings

--- typingtest/settings/prod.py
+++ typingtest/settings/prod.py
@@ -3,5 +3,5 @@ import os
 
 SECRET_KEY = os.environ.get("TYPER_DJSETTINGS_KEY")
 DEBUG = False
-ALLOWED_HOSTS = ['localhost','137.184.142.120',"127.0.0.1"]
+ALLOWED_HOSTS = ['localhost','137.184.142.120',"devtyper.io", "www.devtyper.io"]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
