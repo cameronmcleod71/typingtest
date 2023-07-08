cce36b268747600297e6a1937ce783c986c9126a
Cameron McLeod <cameronmcleod@Camerons-Air.gateway.sasktel.net>
2023-07-08 08:46:01 -0600

    started cursor

--- frontend/src/components/MotionCursor.js
+++ frontend/src/components/MotionCursor.js
@@ -1,6 +1,41 @@
 import React from "react";
-import { motion } from "framer-motion"
+import { motion } from "framer-motion";
 
-export default function MotionCursor() {
-  return ();
+export default function MotionCursor({ ...props }) {
+  const variants = {
+    start: {
+      "&::before": {
+        x: 0,
+      },
+    },
+    end: {
+      "&::before": {
+        x: 0,
+      },
+    },
+  };
+  return (
+    <motion.div
+      variants={variants}
+      initial="start"
+      animate="end"
+      transition={{ duration: 0.15 }}
+      sx={{
+        color: "grey",
+        position: "relative",
+        width: "max-content",
+        "&::before": {
+          content: "",
+          "border-left": "2px solid",
+          "border-color": "grey",
+          "white-space": "nowrap",
+          overflow: "hidden",
+          position: "absolute",
+        },
+      }}
+      {...props}
+    >
+      {children}
+    </motion.div>
+  );
 }
