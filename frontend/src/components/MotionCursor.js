bd34a3af3c03b2a18538760e3c71cfea93601346
Cameron McLeod <cameronmcleod@Camerons-Air.gateway.sasktel.net>
2023-07-08 09:59:15 -0600

    I will now change out animating psuedo elements to just animating the element itself,

--- frontend/src/components/MotionCursor.js
+++ frontend/src/components/MotionCursor.js
@@ -1,41 +1,72 @@
-import React from "react";
-import { motion } from "framer-motion";
+import React, {useEffect} from "react";
+import { motion, isValidMotionProp, useAnimation } from "framer-motion";
+import { chakra, shouldForwardProp } from "@chakra-ui/react";
 
-export default function MotionCursor({ ...props }) {
+const ChakraBox = chakra(motion.div, {
+  /**
+   * Allow motion props and non-Chakra props to be forwarded.
+   */
+  shouldForwardProp: (prop) => isValidMotionProp(prop) || shouldForwardProp(prop),
+});
+
+export default function MotionCursor({isInFocus, currentIndex, numOfChars, isCurrentLine, numOfSpaces, lineNum, children, ...props }) {
+  // const variants = {
+  //   start: {
+  //     "&::before": {
+  //       transform: "translateX(0px)",
+  //     },
+  //   },
+  //   end: {
+  //     "&::before": {
+  //       transform: "translateX(100px)",
+  //     },
+  //   },
+  // };
   const variants = {
     start: {
-      "&::before": {
-        x: 0,
-      },
+      "--before-x": "translateX(0)"
     },
     end: {
-      "&::before": {
-        x: 0,
-      },
+      "--before-x": "translateX(100)"
     },
   };
+
+  const move = useAnimation();
+  useEffect(() => {
+    move.start("end");
+  }, [move]);
+
   return (
-    <motion.div
+    <ChakraBox
+      height="max-content" 
+      width="max-content"
       variants={variants}
       initial="start"
-      animate="end"
-      transition={{ duration: 0.15 }}
+      animate="move"
+      transition={{ duration: 1}}
       sx={{
-        color: "grey",
         position: "relative",
         width: "max-content",
+        height: "max-content",
+        display: "flex",
+        "flex-wrap": "wrap",
+        // "background-color": "red",
         "&::before": {
-          content: "",
+          content: '""',
           "border-left": "2px solid",
           "border-color": "grey",
           "white-space": "nowrap",
           overflow: "hidden",
           position: "absolute",
+          top: "0",
+          // right: "0",
+          bottom: "0",
+          // left: "0"
         },
       }}
       {...props}
     >
       {children}
-    </motion.div>
+    </ChakraBox>
   );
 }
--- frontend/src/components/TestTextLineContainer.js
+++ frontend/src/components/TestTextLineContainer.js
@@ -1,7 +1,7 @@
 import React, { Children, isValidElement, cloneElement, useRef, useEffect } from "react";
 import { Box, HStack, Text } from "@chakra-ui/react";
 import { css } from "@emotion/react";
-import Cursor from "./Cursor.style";
+import MotionCursor from "./MotionCursor";
 import { findDOMNode } from "react-dom";
 import {v4} from "uuid";
 
@@ -31,13 +31,13 @@ export default function TyperTextContainer(props) {
             <HStack>
                 <Text sx={lineNumberStyles} filter={ props.lineNum === props.currentLine.toString() ? "opacity(1)" : "opacity(0.6)" }>{props.lineNum}</Text>
                 <Box sx={typingTextStyles}>
-                    <Cursor isInFocus={props.isInFocus} currentIndex={props.currentIndex} numOfChars={props.testTextLine.reduce(testStateReduce, 0)} isCurrentLine={props.lineNum === props.currentLine.toString()} numOfSpaces={ props.testTextLine[0].space ? props.testTextLine[0].space : 0 } lineNum={props.lineNum}>
+                    <MotionCursor isInFocus={props.isInFocus} currentIndex={props.currentIndex} numOfChars={props.testTextLine.reduce(testStateReduce, 0)} isCurrentLine={props.lineNum === props.currentLine.toString()} numOfSpaces={ props.testTextLine[0].space ? props.testTextLine[0].space : 0 } lineNum={props.lineNum}>
                         {Children.map( props.parentChildren, (child) => {
                             if(!isValidElement(child)) return null;
 
                             return cloneElement(child, { ...child.props, testTextLine: props.testTextLine, currentIndex: props.currentIndex, isCurrentLine: props.currentLine.toString() === props.lineNum});
                         })}
-                    </Cursor>
+                    </MotionCursor>
                 </Box>
             </HStack>
         </Box>
