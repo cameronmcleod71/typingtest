819233cdce43690e351845393a3674acd8b4c006
Cameron McLeod <cameronmcleod@Camerons-Air.gateway.sasktel.net>
2023-07-15 08:30:14 -0600

    added the additional argument --blank, -b to the leaderboard management command

--- typer_api/management/commands/leaderboardjson.py
+++ typer_api/management/commands/leaderboardjson.py
@@ -2,5 +2,12 @@ from django.core.management.base import BaseCommand, CommandError
 
 class Command(BaseCommand):
     
+    def add_arguments(self, parser):
+       parser.add_argument("-b", "--blank", action="store_true", help="Just create a blank object")
+    
     def handle(self, *args, **options):
+      blank = options["blank"]
       print("hello world")
+
+      if blank:
+         print("is blank")
